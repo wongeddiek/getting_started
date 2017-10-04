2539cb754ec15ba67cca7f912642381f020bf574
Edward Wong <wong.eddiek@gmail.com>
2017-10-04 09:45:32 -0600

    added more functions in higherOrderFunctions.hs

--- higherOrderFunctions.hs
+++ higherOrderFunctions.hs
@@ -22,7 +22,7 @@ zipWith' _ [] _ = []
 zipWith' _ _ [] = []
 zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
 
--- ghci> zipWith' (+) [4,2,5,6] [2,6,2,3]  
+-- ghci> zipWith' (+) [4,2,5,6] [2,6,2,3]
 -- [6,8,7,9]
 -- ghci> zipWith' max [6,3,2,1] [7,3,1,5]
 -- [7,3,2,5]
@@ -32,3 +32,63 @@ zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
 -- [2,4,6,8,10]
 -- ghci> zipWith' (zipWith' (*)) [[1,2,3],[3,5,6],[2,3,4]] [[3,2,2],[3,4,5],[5,4,3]]
 -- [[3,4,6],[9,20,30],[10,12,12]]
+
+-- Flip takes a function and returns a function that is like our original function, only the first two arguments are flipped.
+flip' :: (t1 -> t2 -> t3) -> t2 -> t1 -> t3
+-- flip' f = g
+--   where g x y = f y x
+flip' f x y = f y x
+
+-- ghci> flip' zip [1,2,3,4,5] "hello"
+-- [('h',1),('e',2),('l',3),('l',4),('o',5)]
+-- ghci> zipWith (flip' div) [2,2..] [10,8,6,4,2]
+-- [5,4,3,2,1]
+
+-- map takes a function and a list and applies that function to every element in the list, producing a new list.
+map' :: (t -> a) -> [t] -> [a]
+map' _ [] = []
+map' f (x:xs) = f x : map' f xs
+
+-- filter is a function that takes a predicate (a predicate is a function that tells whether something is true or not, so in our case, a function that returns a boolean value) and a list and then returns the list of elements that satisfy the predicate.
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+-- filter' p (x:xs) = if (p x) then x : filter' p xs else filter' p xs
+filter' p (x:xs)
+    | p x       = x : filter' p xs
+    | otherwise = filter' p xs
+
+-- let notNull x = not (null x) in filter' notNull [[1,2,3],[],[3,4,5],[2,2],[],[],[]]
+
+--quicksort using curried function and filter
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+    let smallerSorted = quicksort (filter (<=x) xs)
+        biggerSorted = quicksort (filter (>x) xs)
+    in  smallerSorted ++ [x] ++ biggerSorted
+
+--  find the largest number under 100,000 that's divisible by 3829.
+findNum = head (filter p [100000, 99999..])
+  where p x = x `mod` 3829 == 0
+
+-- find the sum of all odd squares that are smaller than 10,000
+oddSquares = sum (takeWhile (< 10000) (filter odd (map (^2) [1..])))
+--written with list comprehension
+-- sum (takeWhile (<10000) [n^2 | n <- [1..], odd (n^2)])
+
+-- Collatz sequences. We take a natural number. If that number is even, we divide it by two. If it's odd, we multiply it by 3 and then add 1 to that. We take the resulting number and apply the same thing to it, which produces a new number and so on. In essence, we get a chain of numbers. It is thought that for all starting numbers, the chains finish at the number 1
+collatz 1 = [1]
+collatz n
+  | even n = n : collatz (n `div` 2)
+  | odd n  = n : collatz (n * 3 + 1)
+
+-- for all starting numbers between 1 and 100, how many chains have a length greater than 15?
+collatzTest = length (filter p (map collatz [1..100]))
+  where p xs = length xs > 15
+
+
+-- the below maps (*) to a list of numbers starting with 0 and returns a list of functions
+-- [(*0), (*1), (*2)...]
+-- let listOfFuns = map (*) [0..]
+-- calling the index 4 of the above list give you (* 4), passin 5 as the second parameter gives you 20  
+-- (listOfFuns !! 4) 5
