ab9c3919415610f8d2d4ddc57416f1f1cb5a1527
Edward Wong <wong.eddiek@gmail.com>
2017-10-19 18:21:24 -0600

    completed prob 7 & 8

--- misc.hs
+++ misc.hs
@@ -1,3 +1,5 @@
+import Data.List --(\\)
+import Data.List.Ordered (minus, union, unionAll)
 
 -- Given a list, for each element, return a list that contains the all elements from start of the input list to the current element.
 combinator :: [a] -> [[a]]
@@ -33,7 +35,7 @@ gcd' x y
 gcd'' :: Int -> Int -> Int
 gcd'' x 0 = abs x
 gcd'' 0 y = abs y
-gcd'' x y = b (mod a b)
+gcd'' x y = gcd'' b (mod a b)
   where a = abs x
         b = abs y
 
@@ -49,3 +51,30 @@ divisors :: Int -> Int -> [Int]
 divisors x y = filter (\ z -> ((mod x z) + (mod y z)) == 0)  [a, b..1]
   where a = min x y
         b = if (a - 1) < 0 then 0 else (a - 1)
+
+-- Prime sieve - inefficient
+primesTo m = sieve [2..m]       {- (\\) is set-difference for unordered lists -}
+             where
+             sieve (x:xs) = x : sieve (xs \\ [x,x+x..m])
+             sieve [] = []
+
+-- prime sieve - more efficient
+-- infinite prime list.  Uses the Data.List.Ordered module -> minus, unionAll functions
+
+-- The minus function computes the difference of two ordered lists. An element occurs in the output as many times as it occurs in the first input, minus the number of occurrences in the second input. If the first input is a set, then the output is a set.  It computes for infinite lists
+
+-- The unionAll computes the union of a (potentially) infinite number of lists, under the assumption that the heads of the inner lists are sorted. The result will duplicate an element as many times as the maximum number of occurrences in any single list. Thus, the result is a set if and only if every inner list is a set.
+
+--Start with the first 2 primes, next generate a nested list of all odd prime multiples, starting with prime^2 using list comprehension, beginning with 3.  Computes the union of this nested list using 'unionAll'.  Then, using a list of all odd numbers starting 5, compute the difference with the multiple list using 'minus' to remove all the odd prime multiples.
+
+primesList = 2 : 3 : minus [5,7..] (unionAll [[p*p, p*p+p*2..] | p <- tail primesList])
+
+-- primes = 2 : 3 : minus [5,7..] (unionAll [[p*p, p*p+2*p..] | p <- tail primes])
+
+
+-- Generate an infinite prime list
+-- start with 2, attach it with a tail prime list that we'll build.
+-- The tail prime list will start at 3, attach it to a filtered list of odd numbers, where the predicate will take this same tail prime list, and check each element in the odd numbers list to each element in this same tail prime list to see if it's divisble (check prime)
+primesList' = 2 : primes'
+  where isPrime (p:ps) n = p*p > n || n `rem` p /= 0 && isPrime ps n
+        primes' = 3 : filter (isPrime primes') [5, 7 ..]
--- projectEuler.hs
+++ projectEuler.hs
@@ -1,3 +1,5 @@
+import Data.Char (digitToInt) --for Problem 8
+
 -- Problem 1: Multiples of 3 and 5
 -- If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 -- Find the sum of all the multiples of 3 or 5 below n.
@@ -106,3 +108,28 @@ sumSquareDiff :: [Int] -> Int
 sumSquareDiff xs = squaredOfSum xs - sumOfSquared xs
   where squaredOfSum = (^2) . sum
         sumOfSquared = sum . map (^2)
+
+
+-- Problem 7: 10001st prime
+-- What is the 10 001st prime number?
+
+-- function generating an infinite prime list
+-- start with 2, attach it with a tail prime list that we'll build.
+-- The tail prime list will start at 3, attach it to a filtered list of odd numbers, where the predicate will take this same tail prime list, and check each element in the odd numbers list to each element in this same tail prime list to see if it's divisble (check prime).
+-- This can be more efficient.  How can we implement a sieve prime finder?
+primesList' = 2 : primes'
+  where isPrime (p:ps) n = p*p > n || n `rem` p /= 0 && isPrime ps n
+        primes' = 3 : filter (isPrime primes') [5, 7 ..]
+
+
+--Problem 8: Largest product in a series
+-- Find the thirteen adjacent digits in the below 1000-digit number that have the greatest product. What is the value of this product?
+
+longNum = "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450"
+
+-- Grab each consecutive 13-digits number strings from longNum,  pass productNum function -- which coverts each number char to Int and multiply them together, and set them into a new list with the product of each 13-d consec. number.
+
+largestProduct = maximum (numList longNum)
+  where numList []        = []
+        numList (x:xs)    =  productNum (take 13 (x:xs)) : numList xs
+        productNum        = foldl (\acc x -> acc * (digitToInt x)) 1
